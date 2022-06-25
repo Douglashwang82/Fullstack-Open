5321f78636bace34773b99aadbfdbfaf4ee67c01
douglashwang82 <douglas821209@gmail.com>
2022-06-24 17:11:43 -0700

    part1

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,77 @@
+import { useState,useEffect } from 'react'
+
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+]
+
+
+const Vote = ({selected, handlePoints, handleSelected, points}) => {
+  return(
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points} points</p>
+      <button onClick={handlePoints}>vote</button>
+      <button onClick={handleSelected}>next anecdote</button>
+    </div>
+  )
+}
+
+const MostVote = ({most, points}) => {
+  return(
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {
+        points === 0 ?
+      <p>no vote yet</p>:
+      <>
+      <p>{anecdotes[most]}</p>
+      <p>had {points} points</p>
+      </>
+}
+    </div>
+  )
+}
+
+const App = () => {   
+  const initial_points = new Array(anecdotes.length + 1).join('0').split('').map(parseFloat);
+  const [selected, setSelected] = useState(0)
+  const [points ,setPoints] = useState(initial_points);
+  const [most, setMost] = useState(0);
+
+  const getRandomIndex = () => {
+    let temp = Math.floor(Math.random() * anecdotes.length);
+    while (temp === selected) {
+      temp = Math.floor(Math.random() * anecdotes.length);
+    }
+    return temp;
+  }
+  
+  const handleSelected = () => setSelected(getRandomIndex());
+
+  const handlePoints = () => {
+    let copy = {...points};
+    copy[selected] += 1;
+    setPoints(copy);
+  }
+
+  const updateMost = () => {
+    if (points[selected] > points[most]) setMost(selected);
+  }
+  
+  updateMost();
+  return (
+    <div>
+      <Vote handleSelected={handleSelected} handlePoints={handlePoints} selected={selected} points={points[selected]}/>
+      <MostVote most={most} points={points[most]}/>
+    </div>
+  )
+}
+
+export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+const Button = ({handler, children}) => {
+  return (
+    <button onClick={handler}>{children}</button>
+  )  
+}  
+
+const Action = ({handleBad, handleGood, handleNeutral}) => {
+  return(
+    <>
+    <h1>give feedback</h1>
+    <Button handler={handleGood}>good</Button>
+    <Button handler={handleNeutral}>neutral</Button>
+    <Button handler={handleBad}>bad</Button>
+    </>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+return (
+  <>
+  <p>{text} {value}</p>
+  </>
+)
+}
+
+const Statistic = ({good, neutral, bad, all, average, positive}) => {
+
+  return(
+    <div>
+    <h1>statistics</h1>
+    <table>
+      <tbody>
+
+      <tr><td><StatisticLine text="good" value={good} /></td></tr>
+      <tr><td><StatisticLine text="neutral" value={neutral} /></td></tr>
+      <tr><td><StatisticLine text="bad" value={bad} /></td></tr>
+      <tr><td><StatisticLine text="all" value={all} /></td></tr>
+      <tr><td><StatisticLine text="neutral" value={average} /></td></tr>
+      <tr><td><StatisticLine text="positive" value={parseFloat(positive * 100).toFixed(2)+"%"} /></td></tr>
+      </tbody>
+    </table>
+    </div>
+  )
+}
+
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  function countAll(){
+    return good + neutral + bad;
+  }
+
+  function countAvg() {
+    const all = countAll();
+    if (all === 0) return 0;
+    return (good - bad) / all;
+  }
+
+  function countPositive() {
+    const all = countAll();
+    if (all === 0) return 0;
+    return good / countAll();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Action handleBad = {handleBad} handleGood={handleGood} handleNeutral={handleNeutral}/>
+    {countAll() === 0 ? 
+    <p>No feedback given</p> 
+    :
+    <Statistic good={good} neutral={neutral} bad={bad} all={countAll()} average={countAvg()} positive={countPositive()}/>
+    }
     </div>
   );
 }
